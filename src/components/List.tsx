ad939e082de120ec68e79e69ea2a77778cf26a4e
realz <realz2013@gmail.com>
2019-07-07 00:16:26 +0500

    add projects list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import Wrapper from './Wrapper';
 
+const Projects = [
+  'Red Sauce 3',
+  'God of Peace',
+  'Super Farm',
+  'World of Xsolla',
+  'Half-Life 3',
+  'Storefront',
+  'Awesome Game 2',
+  'LiF',
+  'Gemini'
+];
+
 class List extends Component {
   render() {
     return (
       <Wrapper>
-        <div className='List'>ТУТ Список должен быть)</div>
+        <ul className='List'>
+          {Projects.map((entry, index) => (
+            <li key={index}>{entry}</li>
+          ))}
+        </ul>
       </Wrapper>
     );
   }
